6af9687980acc313b055a7c1052412f1c70bff5c
gretmn102 <erlgreywatson@gmail.com>
2023-01-11 14:53:35 +0200

    first prototype

--- src/Program.fs
+++ src/Program.fs
@@ -6,6 +6,11 @@ type ResultComputation () =
         | Ok ok -> f ok
         | Error err -> Error err
 
+    member _.Using(x, f) =
+        match x with
+        | Ok ok -> using ok f
+        | Error err -> Error err
+
     member _.Return x = x
 
 let resultComputation = ResultComputation()
@@ -38,12 +43,13 @@ type CustomDateTime =
         Time: CustomTime option
     }
 
-module DateTimeParser =
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+[<RequireQualifiedAccess>]
+module CustomDateTime =
     module Parser =
         open FParsec
 
         type 'a Parser = Parser<'a, unit>
-
 
         let pdate: _ Parser =
             pipe3
@@ -60,7 +66,7 @@ module DateTimeParser =
 
         let ptime: _ Parser =
             pipe3
-                (puint32)
+                puint32
                 (skipChar ':' >>. puint32)
                 (opt (skipChar ':' >>. puint32))
                 (fun hours mins secs ->
@@ -73,7 +79,7 @@ module DateTimeParser =
 
         let p: _ Parser =
             pipe2
-                (opt (pdate .>> spaces))
+                (opt (attempt (pdate .>> spaces)))
                 (opt ptime)
                 (fun date time ->
                     {
@@ -90,24 +96,35 @@ module DateTimeParser =
     let parse =
         Parser.runResult Parser.p
 
+    let toDateTime (customDateTime: CustomDateTime) =
+        let date = customDateTime.Date
+        let time = customDateTime.Time
+
+        System.DateTime(
+            date |> Option.map (fun x -> int x.Year) |> Option.defaultValue 1,
+            date |> Option.map (fun x -> int x.Month) |> Option.defaultValue 1,
+            date |> Option.map (fun x -> int x.Day) |> Option.defaultValue 1,
+            time |> Option.map (fun x -> int x.Hours) |> Option.defaultValue 0,
+            time |> Option.map (fun x -> int x.Minutes) |> Option.defaultValue 0,
+            time |> Option.bind (fun x -> x.Seconds |> Option.map int) |> Option.defaultValue 0
+        )
+
 type Item =
     {
         /// 1.1, 1.1.1, ...
         Index: int []
         /// в двухзначных индексах стоит дата, которая распределяется по всем остальным
-        Trailer: Choice<CustomDateTime, string>
-        StartDateTime: CustomDateTime
+        Trailer: Choice<CustomDate, string>
+        StartDateTime: System.DateTime
         StartLocation: string
-        EndDataTime: CustomDateTime
+        EndDataTime: System.DateTime
         EndLocation: string
     }
 
-
+[<Struct>]
+type StartOrEnd = Start | End
 
 let start (xmlPath: string) =
-    // let xmlPath = @"e:\downloads\2023-01-10_11-56-48.xlsx"
-    // System.IO.IOException
-
     let getWorkbook (path: string) =
         try
             new XLWorkbook(path)
@@ -119,8 +136,6 @@ let start (xmlPath: string) =
             | e ->
                 sprintf "Непредвиденная ошибка:\n%A" e
                 |> Error
-
-    // use workbook = new XLWorkbook(xmlPath)
 
     let getWorksheet name (workbook: XLWorkbook) =
         match workbook.Worksheets.TryGetWorksheet name with
@@ -140,40 +155,9 @@ let start (xmlPath: string) =
             |> Array.ofSeq
 
     resultComputation {
-        let! workbook = getWorkbook xmlPath
+        use workbook = getWorkbook xmlPath
         let! worksheet = getWorksheet "Поездки" workbook
 
-
-        // worksheet.Rows
-        // let indexes = worksheet.Columns(1, 1)
-        // let прицепы = worksheet.Columns(2, 2)
-        // let startDateTimes = worksheet.Columns(3, 3)
-        // let startLocations = worksheet.Columns(4, 4)
-        // let endDateTimes = worksheet.Columns(6, 6)
-        // let endLocations = worksheet.Columns(7, 7)
-
-
-        let length =
-            // `worksheet.RowCount()` возвращает 1048576, так что даже не надейся
-            // worksheet.Rows()
-            // |> Seq.iter (fun )
-            10 // TODO
-            // indexes.Cells()
-            // |> Seq.findIndex (fun x -> x.Value.IsBlank)
-            // |> fun x -> x + 1
-
-        // Seq.initInfinite id
-        // |> Seq.mapFold
-        //     (fun st x ->
-        //         match st with
-        //         | Some st ->
-        //             if st > 5 then x, None else x, Some (st + 1)
-        //         | None ->
-        //             failwithf ""
-        //     )
-        //     (Some 0)
-
-        // printfn "length = %d" length
         let rows =
             worksheet.Rows()
             |> Seq.skip 1 // пропускает заголовки
@@ -182,23 +166,17 @@ let start (xmlPath: string) =
             )
             |> List.ofSeq
 
-        let items () =
-            let rec f (acc, lastDate: System.DateTime option) = function
+        let getItems () =
+            let rec f (acc, lastDate: CustomDate option) = function
                 | (row: IXLRow)::rows ->
-
                     let trailer = row.Cell(int ColumnHeader.Trailers)
-
-                    // if index.Value.IsBlank then
-                    //     None
-                    // else
-                    // Неизвестно
 
                     let dateOrTrailer =
                         let value = trailer.GetText()
-                        match System.DateTime.TryParse value with
-                        | true, date ->
-                            Choice1Of2 date
-                        | false, _ ->
+                        match CustomDateTime.Parser.runResult CustomDateTime.Parser.pdate value with
+                        | Ok x ->
+                            Choice1Of2 x
+                        | Error _ ->
                             Choice2Of2 value
 
                     let lastDate =
@@ -207,7 +185,22 @@ let start (xmlPath: string) =
                         | _ -> lastDate
 
                     let parseGetDateTime (cell: IXLCell) =
-                        todo
+                        let res =
+                            cell.GetString()
+                            |> CustomDateTime.parse
+
+                        match res with
+                        | Ok customDateTime ->
+                            if Option.isSome customDateTime.Date then
+                                customDateTime
+                            else
+                                { customDateTime with
+                                    Date = lastDate
+                                }
+                            |> CustomDateTime.toDateTime
+
+                        | Error x ->
+                            failwithf "%s" x
 
                     let item =
                         {
@@ -228,33 +221,72 @@ let start (xmlPath: string) =
                                 let endLocations = row.Cell(int ColumnHeader.EndLocations)
                                 endLocations.GetString()
                         }
-                    // System.DateTime.ParseExact "18:34:32"
-                    // printfn "%A" (trailer.Value, trailer.Value.Type)
 
                     f (item::acc, lastDate) rows
                 | [] ->
                     List.rev acc
             f ([], None) rows
-            // |> Seq.mapFold
-            //     (fun (lastDate: System.DateTime) x ->
-            //         let index = x.Cell(int ColumnHeader.Indexes)
-            //         let trailer = x.Cell(int ColumnHeader.Trailers)
-            //         let startDateTime = x.Cell(int ColumnHeader.StartDateTimes)
-            //         let startLocation = x.Cell(int ColumnHeader.StartLocations)
-            //         let endDateTime = x.Cell(int ColumnHeader.EndDateTimes)
-            //         let endLocations = x.Cell(int ColumnHeader.EndLocations)
 
-            //         if index.Value.IsBlank then
-            //             None
-            //         else
+        let items = getItems ()
 
-            //             index.Value.Type
+        let items =
+            items
+            |> List.filter (fun x -> x.Index.Length = 3)
 
-            //     )
-            //     // System.DateTime
-            //     None
-            //     // lastDate
-        items ()
+        let allDates =
+            items
+            |> List.fold
+                (fun st x ->
+                    Set.add x.StartDateTime st
+                    |> Set.add x.EndDataTime
+                )
+                Set.empty
+            |> List.ofSeq
+
+        let table =
+            items
+            |> List.groupBy (fun x -> x.Trailer)
+            |> List.map (fun (trailer, xs) ->
+                match trailer with
+                | Choice2Of2 trailer ->
+                    allDates
+                    |> List.map (fun d ->
+                        xs
+                        |> List.tryPick (fun x ->
+                            if x.StartDateTime = d then
+                                Some x.StartLocation
+                            elif x.EndDataTime = d then
+                                Some x.EndLocation
+                            else
+                                None
+                        )
+                    )
+                    |> fun xs -> Some trailer :: xs
+            )
+            |> fun items ->
+                let allDates =
+                    allDates
+                    |> List.map (fun x -> Some <| x.ToString())
+                (None :: allDates) :: items
+
+        do
+            use workbook = new XLWorkbook()
+
+            let worksheet = workbook.AddWorksheet()
+
+            table
+            |> List.iteri (fun y ->
+                List.iteri (fun x v ->
+                    v
+                    |> Option.iter (fun v ->
+                        let c = worksheet.Cell(y + 1, x + 1)
+                        c.SetValue (XLCellValue.op_Implicit(v))
+                        |> ignore
+                    )
+                )
+            )
+
+            workbook.SaveAs("output.xlsx")
 
         return Ok "Готово!"
     }
